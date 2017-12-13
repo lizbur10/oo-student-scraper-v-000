08302d0589410b885ac4f9b47754819ac0bc4f4e
Liz Burton <liz.burton147@gmail.com>
2017-12-13 22:27:00 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,14 +13,14 @@ class Scraper
     doc = Nokogiri::HTML(html)
     students = doc.css("div.student-card")
     students.each do | student |
-      @student_info = {
+      student_info = {
         :name => student.css("h4.student-name").text,
         :location => student.css("p.student-location").text,
         :profile_url => student.css("a")[0]["href"]
       }
-      @scraped_students << @student_info
+      scraped_students << student_info
     end
-    @scraped_students
+    scraped_students
   end
 
   def self.scrape_profile_page(profile_url)
