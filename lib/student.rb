9207c302b8c652c8c37fba94f252af244d37119a
Liz Burton <liz.burton147@gmail.com>
2017-12-13 22:04:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
   def add_student_attributes(attributes_hash)
     attributes_hash.each do | key, value |
       attribute_name = key.to_s
-      binding.pry
+      # binding.pry
       self.attribute_name = attributes_hash[:value]
     end
     # self.blog = attributes_hash[:blog]
