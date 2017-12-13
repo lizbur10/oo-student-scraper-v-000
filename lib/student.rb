0637f898ec24b80222959a653cf2d1277b01c197
Liz Burton <liz.burton147@gmail.com>
2017-12-13 21:54:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do | student |
-      student = Student.new
+      Student.new(student)
     end
   end
 
