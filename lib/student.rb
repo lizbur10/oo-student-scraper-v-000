850c56614a5b8e0ad7bd2c7059a9469a9c58c202
Liz Burton <liz.burton147@gmail.com>
2017-12-13 21:53:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    students.array.each do | student |
+      student = Student.new
+    end
   end
 
   def add_student_attributes(attributes_hash)
